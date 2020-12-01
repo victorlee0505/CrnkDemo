5e71a64af209811cb95f90eed3e3ffcc118c51a6
Lee <victor.k.lee@ontario.ca>
2020-12-01 16:46:05 -0500

    First Commit

---
+++ src/main/java/com/example/crnkdemo/CrnkdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crnkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrnkdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrnkdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crnkdemo/CrnkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crnkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrnkdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
