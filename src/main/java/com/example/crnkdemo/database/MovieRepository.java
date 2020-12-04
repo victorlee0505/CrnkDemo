aac8f8014d23ba4bc4bfe72f232baf6632375809
Lee <victor.k.lee@ontario.ca>
2020-12-04 13:08:07 -0500

    MovieRepository: crnk Repo from Jpa Entity

---
+++ src/main/java/com/example/crnkdemo/database/MovieRepository.java
@@ -0,0 +1,15 @@
+package com.example.crnkdemo.database;
+
+import com.example.crnkdemo.database.entity.Movie;
+
+import org.springframework.stereotype.Component;
+
+import io.crnk.data.jpa.JpaEntityRepositoryBase;
+
+@Component
+public class MovieRepository extends JpaEntityRepositoryBase<Movie, Long> {
+
+    public MovieRepository() {
+        super(Movie.class);
+    }
+}
