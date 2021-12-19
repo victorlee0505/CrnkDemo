e125882ac13c9fde2322e8961e8ca53a3f698d21
Lee <victor.k.lee@ontario.ca>
2021-12-19 11:51:19 -0500

    enable H2 server mode (DBeaver can access)

--- src/main/java/com/example/crnkdemo/CrnkdemoApplication.java
+++ src/main/java/com/example/crnkdemo/CrnkdemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.crnkdemo;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrnkdemoApplication {
@@ -9,5 +13,10 @@ public class CrnkdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrnkdemoApplication.class, args);
 	}
+
+	@Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
 
 }
